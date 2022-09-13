a245df1de58eb3138a80c796d25f71d491a873d3
Deathstars22 <josgabfer@outlook.com>
2022-09-13 08:57:11 -0600

    getToken module working, missing try catch

--- Modules/Auth/getToken.py
+++ Modules/Auth/getToken.py
@@ -5,6 +5,8 @@ from requests.models import HTTPError
 from requests.auth import HTTPBasicAuth
 import http.client as http_client
 import logging
+import dotenv
+
 
 
 http_client.HTTPConnection.debuglevel = 1
@@ -13,29 +15,56 @@ logging.getLogger().setLevel(logging.DEBUG)
 requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
+dotenv.load_dotenv()
 
-ORG = ''
-KEY = os.getenv('D_KEY')
-SEC = os.getenv('D_SECRET')
 
-def getToken():
+def check_token(token_type):
+    """
+        This module will check if the module exists, it will take a token type
+        Token type options: 
+            A --> Admin
+            D --> Deployments
+            P --> Policies
+            R --> Reports
+        If the token exists, it returns the token to the requester
+        If the token does not exists, it calls generate token with the token type
+    """
+
+    token = token_type + '_' +'TOKEN'
+    if os.getenv(token) != None:
+        print('Token already exists')
+        return os.getenv(token)
+    else:
+        generate_auth_string(token_type)
+
+def generate_auth_string(token_type):
+    """Here we encode the API Secret and Key in base64 format needed for the generate_token() function to request an access token."""
+
+    key = os.getenv(token_type+'_'+'KEY')
+    secret = os.getenv(token_type+'_'+'SECRET')
+
+    auth_string = f"{key}:{secret}"
+
+    message_bytes = auth_string.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_auth = base64_bytes.decode('ascii')
+    token_API_Type = token_type + '_' +'TOKEN'
+
+
+    generate_token(base64_auth, token_API_Type)
+
+
+def generate_token(base64_auth, token_API_Type):
     """
         This module will create a new OAUTH2 token for API Queries, more information at:
         https://developer.cisco.com/docs/cloud-security/#!auth-overview
     """
-    creds = '{KEY}:{SEC}'.format(KEY=KEY, SEC=SEC)
-    URL = 'https://api.umbrella.com/auth/token'
+    url = "https://api.umbrella.com/auth/v2/token"
+    dotenv_file = dotenv.find_dotenv()
     headers = {
-        "Content-Type": "",
-        "Accept": "application/json",
-        "Authorization": "Basic %s" %credsb64
+        "Authorization": "Basic " + base64_auth
     }
-    try:
-        resp = requests.request('POST', URL, headers=headers, data=payload)
-        print(resp.text.encode('utf8'))
-    except HTTPError as httperr:
-        print('Error' + httperr)
-    except Exception as e:
-        print('Error' + e)
-
-    
+    response = requests.request("GET", url, headers = headers)
+    token_json = response.json()
+    token = token_json.get('access_token')
+    dotenv.set_key(dotenv_file,token_API_Type, token)
--- UmbrellaAPI.py
+++ UmbrellaAPI.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import json
-from Modules.Auth.getToken import getToken
+from Modules.Auth.getToken import check_token
 
 from datetime import datetime
 
@@ -16,12 +16,13 @@ parser.add_argument('-k','--key', dest='key',metavar='', type=str, help='This va
 parser.add_argument('-s','--secret', dest='secret', metavar='',type=str, help='This value stores the secret created from your Umbrella Dashboard')
 parser.add_argument('-n','--name', dest='name', metavar='',type=str, help='This value represents the new credentials for the connection')
 parser.add_argument('-p','--path', dest='path', metavar='',type=str, help='This value instructs the program where to save the Database in your OS, for example Windows: C:\\\\path\\\\to\\\\ ---- Unix/Mac: sqlite:////absolute/path/to/foo.db')
-parser.add_argument('-ar','--activity', dest='activity', metavar='',type=str, help='This argument will tell the program to query the Activity Report in Cisco Umbrella')
+parser.add_argument('-t','--token-type', dest='token_type', metavar='',type=str, help='This argument requires an option to set the token type, either (A: Admin, D: Deployments, P: Policies, R: Reports)')
+
 
 
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-S', '--setup', action='store_true', help='Setup the connection settings, it requires an orgainzation ID (-o), a key (-k) and a secret (-s)')
-group.add_argument('-rt', '--requestToken', action='store_true', help='Request a new token')
+group.add_argument('-rt', '--request_token', action='store_true', help='Request a new token')
 
 
 
@@ -34,13 +35,17 @@ def setupConnectionSettings():
         saveConnectionSettings: function in Model.py, this will save the settings in a DB (ConnectionSettings.db) to reuse the settings
     """
     print("Setup Settings")
-
+def check_token_args(token_type):
+    if(token_type != 'A' or token_type != 'D' or token_type != 'P' or token_type != 'R'):
+        print('Invalid option, please use either (A: Admin, D: Deployments, P: Policies, R: Reports)')
+    else:
+        check_token(token_type)
 
 def main():
     if args.setup:
         print("setup")
-    elif args.requestToken:
-        getToken()
+    elif args.request_token:
+        check_token_args(args.token_type)
     else:
         print(f'Usage: UmbrellaAPI.py [-h] [-o] [-k] [-s] [-n] [-p] [-S | -q | -w | -ct]')
 
