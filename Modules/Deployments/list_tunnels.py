13e8d0365426e5062bea1f29191c6f91c6270103
dani232g <dani232gonzalez@gmail.com>
2022-10-07 14:06:35 -0600

    Cleaning tunnels script

--- Modules/Deployments/list_tunnels.py
+++ Modules/Deployments/list_tunnels.py
@@ -1,4 +1,4 @@
-from Auth.getToken import generate_auth_string
+from ..Auth.getToken import generate_auth_string
 import datetime 
 from dotenv import dotenv_values, find_dotenv
 import pandas 
@@ -11,8 +11,8 @@ import flatdict as flat
 
 """User variables - can be changed
 path_name: Here you can specify the path and name of the CSV file that we will use to save the output from the GET request.
-delete_columns: Here you can specify the name of the columns that you don't want to appear in the CSV file, for example: delete_columns = ['client.authentication.parameters.modifiedAt']"""
-path = "C:\\Users\\dgonzal4\\OneDrive - Cisco\\Desktop\\Python\\"
+delete_columns: Here you can specify the name of the colum2ns that you don't want to appear in the CSV file, for example: delete_columns = ['client.authentication.parameters.modifiedAt']"""
+path = "C:\\"
 delete_columns = []
 
 def parse_tunnels(tunnels_json):
@@ -73,33 +73,5 @@ def get_tunnels(token_type):
     except Exception as e:
         print(colored(f'HTPP error occured: {e}','red'))
 
-"""
-def get_tunnels ():
-    #In this function we will send a GET request to get a list of all the tunnels listed in the Umbrella dashboard.
-    #First, we call the authentication module to retrieve the access token that will be used in the GET request.
-    #If the response status code is 401 or 403 it means that the token is expired or invalid. We generate a new token and call the function get_tunnels() again.
-    #If the response is successful (status code 200) then we use the flatdict package to flat the dictionary stored in tunnels_json. Here we check as well if there are any columns to be removed from the final CSV file.
-    try:
-        token = authentication.retrieve_token()
-        url = "https://api.umbrella.com/deployments/v2/tunnels"
-        headers = {
-            "Authorization": "Bearer " + token,
-            "Content-Type": "application/json"
-        }
-        response = requests.request("GET", url, headers = headers)
-        if (response.status_code == 401 or response.status_code == 403):
-            authentication.generate_token()
-            return get_tunnels()
-        elif response.status_code == 200:
-            tunnels_json = response.json()
-            for item in range(len(tunnels_json)):
-                tunnels_json[item] = flat.FlatDict(tunnels_json[item], '.')
-                if delete_columns:
-                    for delete in delete_columns:
-                        tunnels_json[item].pop(delete)
-            return pandas.DataFrame(tunnels_json)
-    except:
-        print('GET request failed')
-"""
 if (__name__ == "__main__"):
     get_tunnels("X")
