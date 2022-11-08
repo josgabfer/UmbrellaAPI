742f8bc79c56a4d1d70c14b3b635407b52815057
Deathstars22 <josgabfer@outlook.com>
2022-11-08 08:51:26 -0600

    fixing default profile

--- UmbrellaAPI.py
+++ UmbrellaAPI.py
@@ -63,15 +63,16 @@ def check_profile(profile):
 
 def argument_router(args):
     """This function will read the arguments entered, and redirect to any given module as required"""
-    if args.profile == None:
-        print(colored("Entra","red"))
-        with open ("config.json","r") as file:
-            config = json.load(file)
-        jsondumpsprofile = json.dumps(json.dumps(config['DEFAULT_PROFILE']))
-        jsonloadprofile = json.loads(jsondumpsprofile)
-        cleandata = jsonloadprofile.replace('"','')
-        args.profile = cleandata
-        print(args.profile)
+    if not args.setup:
+        if args.profile == None:
+            print(colored("Entra","red"))
+            with open ("config.json","r") as file:
+                config = json.load(file)
+            jsondumpsprofile = json.dumps(json.dumps(config['DEFAULT_PROFILE']))
+            jsonloadprofile = json.loads(jsondumpsprofile)
+            cleandata = jsonloadprofile.replace('"','')
+            args.profile = cleandata
+            print(args.profile)
 
     if args.create:
         if args.auth:
